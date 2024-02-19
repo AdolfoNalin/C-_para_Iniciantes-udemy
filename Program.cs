cf9f2417cf6c5f71359430fbf9e0e6e9aacb0973
AdolfoNalin <nalinadolfo33@gamil.com>
2024-02-19 13:17:38 -0300

    Primeiro código C#
    Aprendendo como funciona a quebra de linha e WriteLine

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Para_Iniciantes
+{
+    class Program
+    {
+        static void Main(String[] angs)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("My name is Adolfo Nalin Junior");
+        }
+    }
+}
