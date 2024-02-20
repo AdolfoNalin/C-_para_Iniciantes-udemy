7b706888e22755e56fd969033d6d916074b9dbed
AdolfoNalin <nalinadolfo33@gamil.com>
2024-02-20 12:50:06 -0300

    Constantes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,15 +11,8 @@ namespace C_Para_Iniciantes
     {
         static void Main(String[] angs)
         {
-            // Forma Priguiçosa
-            var cor_favorito = "Preto";
-            var modeloDeProduto = 022983934;
-
-            // Forma Dinamica, pode mudar o tipo, isso varia do valor que está sendo armazenado
-            dynamic nome = "Adofo nalim";
-            Console.WriteLine(nome);
-            nome = 3000.0f;
-            Console.WriteLine(nome);
+            // Uma costante é somente para valores fixos
+            const float PI = 3.14f; 
         }
     }
 }
