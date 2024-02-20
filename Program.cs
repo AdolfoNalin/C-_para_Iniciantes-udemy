ebb11d5370c5a1fafd3513a4407081fb23d74083
AdolfoNalin <nalinadolfo33@gamil.com>
2024-02-20 12:06:48 -0300

    Declarando variável e tipos

--- Program.cs
+++ Program.cs
@@ -10,8 +10,31 @@ namespace C_Para_Iniciantes
     {
         static void Main(String[] angs)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("My name is Adolfo Nalin Junior");
+            // int Armazena valores interios positivois ou negativos
+            // float Armazena valores de ponto flutuante
+            // Double Armazena valores de pronto flutuante
+            // bool True or char
+            // Sting Armazena texto  "Adolfo"
+            // char Armazena Caracteres
+
+            // Criando Variáveis
+            int anoSegundaGerraMundial = 1942;
+            string cor_favorita = "Preto";
+            float velocidadeF1 = 294.48f; 
+            bool SegundaGerraMundialAconteceu = true;
+
+            // Mostrando Variáveis
+            Console.WriteLine(anoSegundaGerraMundial);
+            Console.WriteLine(cor_favorita);
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine(SegundaGerraMundialAconteceu);
+
+            // Alterando variaveis
+            velocidadeF1 = 348.29f;
+            cor_favorita += "\nAzul";
+
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine(cor_favorita);
         }
     }
 }
