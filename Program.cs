ffaa87099d441a9d2d4616a842d978b5a83c0109
AdolfoNalin <nalinadolfo33@gamil.com>
2024-03-20 20:49:17 -0300

    Atividade: Módulo 1 - exercício 3 - Funções

--- Program.cs
+++ Program.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using C_Para_Iniciantes;
 
 namespace C_Para_Iniciantes
 {
     class Program
     {
+
+        public void PrecoFinal(string nome, decimal valorInicial)
+        {
+            decimal valorFinal = valorInicial;
+            valorFinal += (25 * valorInicial) / 100;
+            
+            Console.WriteLine($"{nome} Valor R${valorFinal}");
+        }
+
         static void Main(String[] angs)
         {
-            int numeroQualquer = 20 + 20 - 80;
-            int numeroMult = 10 * 90;
-            float numeroDiv = 5.0f / 2;
-
-            int teste = (2 + 2) * 10;
+            Program program = new Program();
+            program.PrecoFinal("Chocolate", 30);
+            program.PrecoFinal("Refrigerante", 50);
+            program.PrecoFinal("Bolo", 45);
+            program.PrecoFinal("Sorvete", 60);
+            program.PrecoFinal("Pão", 15);
         }
     }
 }
