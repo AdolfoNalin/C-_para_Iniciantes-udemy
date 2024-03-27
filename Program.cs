9623150def43e72d352bf9cecbe90fb31b4ad144
AdolfoNalin <nalinadolfo33@gamil.com>
2024-03-27 11:02:08 -0300

    Funções
    Prendedo o Math.ABS();

--- Program.cs
+++ Program.cs
@@ -11,31 +11,27 @@ namespace C_Para_Iniciantes
 {
     class Program
     {
-
-       
-
         static void Main(String[] angs)
         {
-            Console.WriteLine("Por favor digite a sua idade!");
-            int idade = int.Parse(Console.ReadLine());
+            GerarPreco(-10);
+        }
 
-        
-            if(idade > 0 && idade <= 10)
-            {
-                Console.WriteLine("Você é uma criança");
-            }
-            else if(idade > 10 && idade < 18)
-            {
-                Console.WriteLine("Você é um adolecente");
-            }
-            else if (idade >= 18 && idade < 60)
-            {
-                Console.WriteLine("você é um adulto");
-            }
-            else{
-                Console.WriteLine("Você é um idoso");
-            }
-        
+        static void ExibirMSG()
+        {
+            Console.WriteLine("Esse sistema é Show de bola");
+            Console.WriteLine("Estou usando funções");
+            Console.WriteLine("Bem vindo");
+        }
+
+        static void GerarPreco(decimal valor)
+        {
+           if (valor < 0)
+           {
+                valor = Math.Abs(valor);
+           }
+
+           decimal valorFinal = valor + (2 * valor);
+           Console.WriteLine("O valor final: " + valorFinal);
         }
     }
 }
