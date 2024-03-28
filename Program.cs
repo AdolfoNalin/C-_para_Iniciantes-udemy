6fa9ff126383e8c2cbec2be1f72a4c9a41945773
AdolfoNalin <nalinadolfo33@gamil.com>
2024-03-28 09:50:30 -0300

    Pensando de outras formas de usar o Enum junto com Funções

--- Program.cs
+++ Program.cs
@@ -13,34 +13,41 @@ namespace C_Para_Iniciantes
 {
     class Program
     {
-        enum Opcao {Criar = 1, Deletar, Editar, Listar, Atualizar};
-        static void Main(String[] angs)
+
+        static int Menu()
         {
             Console.WriteLine("Selecione uma das Opções abaixo:");
             Console.WriteLine("1-Criar\n2-Deletar\n3-Editar\n4-Listrar\n5-Atualizar");
             int index = int.Parse(Console.ReadLine());
+            return index;
+        }
 
-            Opcao selectOp = (Opcao) index;
+        enum Opcao {Criar = 1, Deletar, Editar, Listar, Atualizar};
+        static void Main(String[] angs)
+        {
+            
+            Opcao selectOp = (Opcao) Menu();
 
             switch(selectOp)
             {
                 case Opcao.Criar:
-                    Console.WriteLine();
+                    Console.WriteLine("O que você deseja Criar?");
                     break;
                 case Opcao.Deletar:
-                    Console.WriteLine();
+                    Console.WriteLine("O que você deseja elimina?");
                     break;
                 case Opcao.Editar:
-                    Console.WriteLine();
+                    Console.WriteLine("O que você deseja Editar?");
                     break;
                 case Opcao.Listar:
-                    Console.WriteLine();
+                    Console.WriteLine("O que você deseja Listar?");
                     break;
                 case Opcao.Atualizar:
-                    Console.WriteLine();
+                    Console.WriteLine("O que você deseja Atualizar?");
                     break;
                 default:
                     Console.WriteLine("Essa opção não existe! verifique o menu novamente!");
+                    selectOp = (Opcao) Menu();
                     break;
             }
         } 
