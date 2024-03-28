9cbcae4284c038d7216ad1965d94661ac4f3d166
AdolfoNalin <nalinadolfo33@gamil.com>
2024-03-28 10:43:23 -0300

    While

--- Program.cs
+++ Program.cs
@@ -13,42 +13,13 @@ namespace C_Para_Iniciantes
 {
     class Program
     {
-
-        static int Menu()
-        {
-            Console.WriteLine("Selecione uma das Opções abaixo:");
-            Console.WriteLine("1-Criar\n2-Deletar\n3-Editar\n4-Listrar\n5-Atualizar");
-            int index = int.Parse(Console.ReadLine());
-            return index;
-        }
-
-        enum Opcao {Criar = 1, Deletar, Editar, Listar, Atualizar};
         static void Main(String[] angs)
         {
-            
-            Opcao selectOp = (Opcao) Menu();
-
-            switch(selectOp)
+            int contador = 0;
+            while (contador < 10)
             {
-                case Opcao.Criar:
-                    Console.WriteLine("O que você deseja Criar?");
-                    break;
-                case Opcao.Deletar:
-                    Console.WriteLine("O que você deseja elimina?");
-                    break;
-                case Opcao.Editar:
-                    Console.WriteLine("O que você deseja Editar?");
-                    break;
-                case Opcao.Listar:
-                    Console.WriteLine("O que você deseja Listar?");
-                    break;
-                case Opcao.Atualizar:
-                    Console.WriteLine("O que você deseja Atualizar?");
-                    break;
-                default:
-                    Console.WriteLine("Essa opção não existe! verifique o menu novamente!");
-                    selectOp = (Opcao) Menu();
-                    break;
+                Console.WriteLine("Rodando o While");
+                contador++;
             }
         } 
     }
