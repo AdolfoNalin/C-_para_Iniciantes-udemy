e731edca93289cdfb9bc35538b3993c07bce993a
AdolfoNalin <nalinadolfo33@gamil.com>
2024-04-02 10:13:21 -0300

    Listas

--- Program.cs
+++ Program.cs
@@ -13,42 +13,16 @@ namespace C_Para_Iniciantes
 {
   class Program 
   {
-    struct Produto
-    {
-     
-      public string Nome;
-      public float Preco;
-      public float Peso;
-      public string Marca;
-
-      public Produto(string nome,float preco, float peso, string marca)
-      {
-        this.Nome = nome;
-        this.Preco = preco;
-        this.Peso = peso;
-        this.Marca = marca;
-      }
-
-      public float AddCupon(float porc)
-      {
-         float desconto = this.Preco * porc / 100f;
-         return this.Preco - desconto;
-      }
-
-      public void ExibirInfo()
-      {
-        Console.WriteLine($"Nome: {this.Nome} \nPreço: R${this.Preco} \nPeso: {this.Peso} \nMarca: {this.Marca}");
-      } 
-    }
-
     static void Main(String[] angs)
     {
-      Produto bola = new Produto("Celular", 1500.00f, 0.700f, "Iphone");
+      List<string> clientes = new List<string>();
 
-      bola.ExibirInfo();
-      float valorFinal = bola.AddCupon(50f);
+      clientes.Add("Adolfo");
 
-      Console.WriteLine($"Preço Final: R${valorFinal}");
+      string nome = "Cleiton";
+      clientes.Add(nome);
+
+      Console.WriteLine(clientes[0]);
     }
   }
 }
