dd8b5e410b3f5c653650db77b044182c17d74ded
AdolfoNalin <nalinadolfo33@gamil.com>
2024-04-03 10:43:19 -0300

    Escrita de arquivos de texto
    Conseitos básicos do StreamWriter and File.AppendText

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using C_Para_Iniciantes;
+using System.IO;
 
 namespace C_Para_Iniciantes
 {
@@ -15,35 +16,13 @@ namespace C_Para_Iniciantes
   {
     static void Main(String[] angs)
     {
-      List<string> clientes = new List<string>();
-
-      clientes.Add("Adolfo");
-      clientes.Add("Renam");
-      clientes.Add("Otávio");
-      clientes.Add("Adolfo Nalin Junior");
-      clientes.Add("Josemar");
-      clientes.Add("Rafaela");
-      clientes.Add("Ana júlia");
-
-
-      // foreach(string cliente in clientes)
-      // {
-      //   Console.WriteLine(cliente);
-      // }
+      //StreamWriter escritor = new StreamWriter("teste.txt"); => Para ecrever arquivos novos
+      StreamWriter escritor = File.AppendText("teste.txt"); // Para editar arquivos que já existe e manter os dados
       
-      string FindBuscar = clientes.Find(cliente => cliente == "Josemar");
-      string FindMais10 = clientes.Find(cliente => cliente.Length > 10); // Retorna o primeiro resultado
-      List<string> FindAllMais5 = clientes.FindAll(cliente => cliente.Length > 5); // Retorna o primeiro resultado
+      escritor.WriteLine("Udemy");
+      escritor.Close();
 
-      Console.WriteLine(FindBuscar);
-      Console.WriteLine(FindMais10);
-
-      Console.WriteLine("==========================================================");
-
-      foreach (string item in FindAllMais5)
-      {
-        Console.WriteLine(item);
-      }      
+      Console.WriteLine("Arquivo salvo e criado");
     }
   }
 }
