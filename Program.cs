b5cc0d0d425781fb1b469db716b24d8caad91d0a
AdolfoNalin <nalinadolfo33@gamil.com>
2024-04-09 11:02:45 -0300

    Da cabeça para o papel

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
@@ -12,56 +13,19 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Reflection;
 using System.Security.Cryptography;
+using C__ParaIniciantes;
 
 namespace C_Para_Iniciantes
 {
-  // Exemplo de classificação
-  class Game
-  {
-    #region Atributos ou Campos
-    public string titulo;
-    public string genero;
-    public string produtora;
-    public float preco;
-    #endregion
-
-    #region Metodos
-    public void Abrir()
-    {
-      Console.WriteLine("Abrindo...");
-    }
-
-    public void Carregando()
-    {
-      Console.WriteLine("Carregando...");
-    }
-  
-    public void Executar()
-    {
-      Console.WriteLine("Executando...");
-    }
-
-    public void Fechar()
-    {
-      Console.WriteLine("Fechando...");
-    }
-    #endregion
-  }
   class Program 
   {
     static void Main(String[] angs)
     {
-      // Invocando a classe
-      Game CS = new Game();
-      CS.titulo = "Conter Strick 2";
-      CS.genero = "Ação";
-      CS.produtora = "Valve";
-      CS.preco = 30.00f;
+      Filme MMF = new Filme();
 
-      CS.Abrir();
-      CS.Carregando();
-      CS.Executar();
-      CS.Fechar();
+      MMF.nome = "Meu Malvado Favorito";
+
+      MMF.Executar();
     }
   }
 }
