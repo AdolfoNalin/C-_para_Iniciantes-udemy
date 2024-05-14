7d874b7550ef459df2a4f67394a739ed151b37f0
AdolfoNalin <nalinadolfo33@gmail.com>
2024-05-14 14:31:30 -0300

    Strucs VS Class - Gerenciamento de Memoria

--- Program.cs
+++ Program.cs
@@ -2,18 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using C_para_Iniciantesudemy;
+using C__ParaIniciantes;
 using C_para_Iniciantesudemy.Objects.Formas;
 
 namespace C_para_Iniciantesudemy
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
-            Drawble shape = new Line();
+            Filme filme1 = new Filme(1, nome: "Era do Gelo",descricao: "...", 2004, "");
+            Filme filme2 = filme1;
+            Filme filme3 = filme2;
 
-            shape.Draw();
+            Console.WriteLine(filme3.nome);
+
+            filme1.nome = "Irom man";
+
+            Console.WriteLine(filme3.nome);
+
+            filme3.nome = "Meu malvado favorito!";
+
+            Console.WriteLine(filme2.nome);
         }
     }
 }
